7fcca5fe524bb60390fe5e534235dc0185fc3c20
Manuel Sidler <manuel.sidler@outlook.com>
2024-02-21 11:23:48 +0100

    add more samples for categories and session data

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,6 +1,10 @@
 Office.onReady(() => {
   document.getElementById("tokenButton").onclick = getAccessToken;
   document.getElementById("selectedItemsButton").onclick = getSelectedItems;
+  document.getElementById("itemIdButton").onclick = getItemId;
+  document.getElementById("getMasterCategoriesButton").onclick = getMasterCategories;
+  document.getElementById("saveSessionDataButton").onclick = saveSessionData;
+  document.getElementById("getSessionDataButton").onclick = getSessionData;
 });
 
 function getAccessToken() {
@@ -15,6 +19,38 @@ function getAccessToken() {
 
 function getSelectedItems() {
   Office.context.mailbox.getSelectedItemsAsync(function (result) {
+    if (result.status !== Office.AsyncResultStatus.Succeeded) {
+      setOutput(result.error);
+    } else {
+      setOutput(result.value);
+    }
+  });
+}
+
+function getItemId() {
+  setOutput(Office.context.mailbox.item.itemId ?? "undefined");
+}
+
+function getMasterCategories() {
+  Office.context.mailbox.masterCategories.getAsync(function (result) {
+    if (result.status !== Office.AsyncResultStatus.Succeeded) {
+      setOutput(result.error);
+    } else {
+      setOutput(result.value);
+    }
+  });
+}
+
+function saveSessionData() {
+  Office.context.mailbox.item.sessionData.setAsync("test", "value", function (result) {
+    if (result.status !== Office.AsyncResultStatus.Succeeded) {
+      setOutput(result.error);
+    }
+  });
+}
+
+function getSessionData() {
+  Office.context.mailbox.item.sessionData.getAsync("test", function (result) {
     if (result.status !== Office.AsyncResultStatus.Succeeded) {
       setOutput(result.error);
     } else {
